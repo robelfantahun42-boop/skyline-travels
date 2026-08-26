75504c7fcf37eaaa129b75d1f670f583ac88cd98
robelfantahun42-boop <robelfantahun42@gmail.com>
2026-08-26 06:27:40 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname)));
 
 let applicantsData = [];
 let settingsData = { whatsapp: "251900000000" };
+
+// HTML Routes
+app.get('/admin', (req, res) => {
+  res.sendFile(path.join(__dirname, 'admin.html'));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 // Admin Login API
 app.post('/api/admin/login', (req, res) => {
@@ -32,7 +44,7 @@ app.post('/api/register', (req, res) => {
   }
 });
 
-// Get Applicants
+// Get Applicants API
 app.get('/api/applicants', (req, res) => {
   res.json(applicantsData);
 });
